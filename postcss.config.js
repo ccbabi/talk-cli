9bcca70560348d78af51a61dd391141430a35b03
ccbabi <kxxw28@gmail.com>
2018-07-06 11:32:49 +0800

    fix toRemOption

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
+const defaultConfig = require('./config/default')
 const { getConfig } = require('./config')
 const config = getConfig()
+
 const plugins = [
   require('autoprefixer')({ browsers: config.browsers })
 ]
 
 if (config.toRem) {
-  plugins.push(require('postcss-pxtorem')(config.toRemOption))
+  const toRemOption = { ...defaultConfig.toRemOption, ...config.toRemOption }
+  plugins.push(require('postcss-pxtorem')(toRemOption))
 }
 
 module.exports = { plugins }
